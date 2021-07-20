7200a46ffa46dca1d0237378d96380bb59c8c6c8
odoobot007 <odoobot007@gmail.com>
2021-07-20 09:55:42 +0300

    [ADD] Uploading Paid Module

---
+++ odoo_development/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Development',
+    'version': '14.0.1.1.0',
+    'summary': 'Odoo Tutorials',
+    'sequence': -100,
+    'description': """Odoo Tutorials By Odoo Mates""",
+    'category': 'Tutorials',
+    'author': 'Odoo Bot',
+    'maintainer': 'Odoo Bot',
+    'price': '10.0',
+    'currency': 'USD',
+    'website': 'https://www.odoomates.tech',
+    'license': 'LGPL-3',
+    'depends': [
+        'website_slides',
+    ],
+    'data': [
+        'data/data.xml',
+        'data/slide_data_v12.xml',
+        'data/slide_data_v13.xml',
+        'data/slide_data_v14.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
